a9d372064b6fe863bd3102fd1dfbcc3e15b991bc
Joseph Lim <joseph.d.lim@gmail.com>
2019-02-18 15:15:12 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,14 @@
 // Code The Spaceship Component Here
+
+export default class Spaceship extends React.component {
+  
+  
+  render(
+    <div>
+      Name: {name}
+      Speed: {speed}
+      Rockets?: {hasRockets}
+      Color: {colors}
+    </div>
+    )
+};
